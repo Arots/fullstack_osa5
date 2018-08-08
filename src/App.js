687e507a2b0515e65701be13be986480feec4e52
Aro <otso.aro@cgi.com>
2018-08-08 16:00:58 +0300

    login form

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: []
+      blogs: [],
+      user: null
     }
   }
 
@@ -19,10 +20,15 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h2>blogs</h2>
-        {this.state.blogs.map(blog => 
-          <Blog key={blog._id} blog={blog}/>
-        )}
+        {this.state.user === null ? 
+        <loginForm /> :
+        <div>
+          <h2>blogs</h2>
+          {this.state.blogs.map(blog => 
+            <Blog key={blog._id} blog={blog}/>
+          )}
+        </div>
+        }
       </div>
     );
   }
---
+++ src/components/loginForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const loginForm = ({ user, password }) => {
+    return (
+        <div>
+            <h2>Log in to application </h2>
+            <form onSubmit={} >
+                <input value={} onChange={} />
+                <input value={} onChange={} />
+                <button type='submit' >Login </button>
+            </form>
+        </div>
+    )
+}
+
+export default loginForm
